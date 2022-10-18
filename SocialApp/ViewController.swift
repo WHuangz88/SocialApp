82ab33af6acb7dd96b5528d1ecb9e2d6028b0415
William Huang <william.huang@ajaib.co.id>
2022-10-18 12:59:43 +0700

    Initial Commit

---
+++ SocialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialApp
+//
+//  Created by William Huang on 18/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
